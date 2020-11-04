17577b61c5ac166b0f7f1fc1d1f9b72b49251c07
Ruoqian Cheng <ruoqian.cheng@RuoqiandeMacBook-Pro.local>
2020-11-04 18:26:43 +0800

    [evacheng]feat: added some exception messages

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/dto/User.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/dto/User.java
@@ -13,11 +13,11 @@ public class User {
 
     private Integer id;
     @NotBlank(message = "username must not be blank")
-    @Pattern(regexp ="[0-9A-Za-z_]{3,10}", message = "username must includes only numbers, characters or lower dash with a length of 3-10")
+    @Pattern(regexp = "[0-9A-Za-z_]{3,10}", message = "username must includes only numbers, characters or lower dash with a length of 3-10")
     private String username;
     @NotBlank(message = "password must not be blank")
-    @Size(min = 5, max = 12)
+    @Size(min = 5, max = 12, message = "password must have 5-12 characters")
     private String password;
-    @Email
+    @Email(message = "not a valid email address")
     private String email;
 }
